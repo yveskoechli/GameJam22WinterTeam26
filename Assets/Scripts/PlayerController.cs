8017767e5d35c79fd86657b97ed4488c4a4883cf
yveskoc <yves.koechli@bluewin.ch>
2022-12-12 12:42:53 +0100

    Grundaufbau

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,17 @@
+
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    #region Inspector
+
+    
+
+    #endregion
+
+    #region Unity Event Functions
+
+    
+
+    #endregion
+}
